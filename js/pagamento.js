c7bba0b01470a9898eddf9715129d10a439b2236
Desenvolvedor <enzo.patti@aluno.senai.br>
2023-11-21 15:51:02 -0300

    Máximo de letras pagamento adicionado

--- js/pagamento.js
+++ js/pagamento.js
@@ -1,31 +1,23 @@
-;(function($) {
-    $(function() {
-      $('nav ul li > a:not(:only-child)').click(function(e) {
-        $(this)
-          .siblings('.nav-dropdown')
-          .slideToggle()
-        $('.nav-dropdown')
-          .not($(this).siblings())
-          .hide()
-        e.stopPropagation()
-      })
-      $('html').click(function() {
-        $('.nav-dropdown').hide()
-      })
-  
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active')
-      })
-    })
-  })(jQuery)
-  function darkMode() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-  }
- 
+(function ($) {
+  $(function () {
+    $("nav ul li > a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").slideToggle();
+      $(".nav-dropdown").not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
+    });
+
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
+    });
+    $("#nav-toggle").on("click", function () {
+      this.classList.toggle("active");
+    });
+  });
+})(jQuery);
+
 function meuDark() {
   let body = document.body;
   body.classList.toggle("dark-mode");
@@ -34,11 +26,27 @@ function meuDark() {
   let rodape = document.getElementById("rod");
   rodape.classList.toggle("dark-mode");
 }
-let buttonFunc = document.getElementById('botCont');
+let buttonFunc = document.getElementById("botCont");
 
-buttonFunc.onclick = function(e) {
+buttonFunc.onclick = function (e) {
   e.preventDefault();
-  location.href = 'conclusao.html';
- 
+  location.href = "conclusao.html";
+};
 
+$("input.cvv-input").attr("maxLength", "3").keypress(limitMe);
+
+function limitMe(e) {
+  if (e.keyCode == 8) {
+    return true;
+  }
+  return this.value.length < $(this).attr("maxLength");
+}
+
+$("input.card-number-input").attr("maxLength", "12").keypress(limitMe);
+
+function limitMe(e) {
+  if (e.keyCode == 8) {
+    return true;
+  }
+  return this.value.length < $(this).attr("maxLength");
 }
